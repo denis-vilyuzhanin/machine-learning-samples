4e8fe89f4ed49fe8cc0b178a326aa721032542ce
Denys Viliuzhanin <dvilyuzhanin@gmail,com>
2016-12-17 20:37:20 +0200

    Function Model

--- public/app/computation.js
+++ public/app/computation.js
@@ -1,5 +1,37 @@
 
+
+(function() {
+
 const DERIVATIVE_STEP = 0.00000001;
+
+function FunctionDefinition() {
+}
+
+function FunctionDefinition(arg1, arg2) {
+	
+}
+FunctionDefinition.prototype.specify = function(parameters) {
+	return null;
+}
+FunctionDefinition.prototype.evaluate = function(arguments) {
+	return null;
+}
+
+function FunctionEvaluation(definition) {
+	
+}
+
+
+
+var func = DefineFunction(['a', 'b','c', 'x[2]'], function(a, b, c, x){
+	return a * x[0] + b * x[1] + c;
+});
+
+func.;
+
+var func1 = func.specify({a: 10, b: 20, c:12});
+func.evaluate().set('x', [10, 20]).result();
+
 
 function ComputeDerivative(func, argument, index) {
 	if (Array.isArray(argument)) {
@@ -16,23 +48,53 @@ function ComputeDerivative(func, argument, index) {
 
 
 
-function SquareLoss(options) {
-	this._func = options.func;
-	this._result = 0;
+function SquareLoss(func) {
+	this._func = func;
+	this._totalLoss = 0;
+	this._totalDerivatives = [];
+	this._count = 0;
 }
-SquareLoss.prototype.update = function(argument, target) {
-	this._result += Math.pow(target - this._func(argument), 2);
-}
-SquareLoss.prototype.result = function() {
-	return this._result;
-}
-
-
-if (typeof module !== 'undefined' && module.exports) {
-	module.exports = {
-		DERIVATIVE_STEP: DERIVATIVE_STEP,
-		derivative: ComputeDerivative
+SquareLoss.prototype.update = function(args, target) {
+	var squareLossFunction = (x) => {
+		return Math.pow(target - this._func(x), 2);
+	};
+	var loss = squareLossFunction(args);
+	this._totalLoss += loss;
+	
+	var lossDerivatives = [];
+	for(var i = 0; i < args.length; i++) {
+		lossDerivatives[i] = ComputeDerivative(squareLossFunction, args, i);
+		if (this._totalDerivatives.length < i + 1) {
+			this._totalDerivatives.push(0);
+		}
+		this._totalDerivatives[i] += lossDerivatives[i]; 
+	}
+	this._count++;
+	return {
+		loss: loss,
+		derivatives: lossDerivatives
 	};
 }
+SquareLoss.prototype.total = function() {
+	return this._totalLoss / this._count;
+}
 
+SquareLoss.prototype.derivatives = function() {
+	return this._totalDerivatives;
+}
+
+var exports = {
+	DERIVATIVE_STEP: DERIVATIVE_STEP,
+	derivative: ComputeDerivative,
+	loss: {
+		Square: SquareLoss 
+	}
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+	module.exports = exports; 
+}
+
+
+})();
 
--- public/app/test/computation.test.js
+++ public/app/test/computation.test.js
@@ -5,7 +5,7 @@ const computation = require('../computation');
 
 describe('computation', () => {
 	describe('derivative', () => {
-		it('y=const', () => {
+		it('y = const', () => {
 			function func(x) {
 				return 10;
 			}
@@ -68,13 +68,62 @@ describe('computation', () => {
 			equalFloat(computation.derivative(func, [3, 2, 5], 1), 3 * 3);
 			equalFloat(computation.derivative(func, [3, 2, 5], 2), 1);
 		});
+		
+		it('sigmoid y = 1 / (1 + exp(-x))', () => {
+			function sigmoid(arg) {
+				return 1 / (1 + Math.exp(-arg));
+			}
+			
+			equalFloat(computation.derivative(sigmoid, -10), sigmoid(-10) * (1 - sigmoid(-10)));
+			equalFloat(computation.derivative(sigmoid, 0), sigmoid(0) * (1 - sigmoid(0)));
+			equalFloat(computation.derivative(sigmoid, 0), 0.25);
+			equalFloat(computation.derivative(sigmoid, 100), sigmoid(100) * (1 - sigmoid(100)));
+		});
 	});
 	
+	describe('square loss', () => {
+		describe ('y = a * x + b', () => {
+			function func(args) {
+				return args[0] * args[1] + args[2];
+			}
+			
+			
+			it ('target = actual', () => {
+				var loss = new computation.loss.Square(func);
+				equalLoss(loss.update([2, 3, 1], func([2, 3, 1])), 
+						  {loss: 0.0, derivatives: [0.0, 0.0, 0.0]});
+				equalLoss(loss.update([3, 2, 2], func([3, 2, 2])), 
+						  {loss: 0.0, derivatives: [0.0, 0.0, 0.0]});
+				equalFloat(loss.total(), 0.0);
+			});
+			
+			it ('target = actual + 0.2', () => {
+				var loss = new computation.loss.Square(func);
+
+				const a1 = 2, x1 = 3, b1 = 1, error1 = 0.2;
+				equalLoss(loss.update([a1, x1, b1], func([a1, x1, b1]) + error1), 
+						  {loss: error1 * error1, derivatives: [-2 * x1 * error1, -2 * a1 * error1,  -2 * error1]});
+				
+				const a1 = 2, x1 = 3, b1 = 1, error1 = 0.2;
+				/*equalLoss(loss.update([3, 2, 2], func([3, 2, 2]) + 0.2), 
+						  {loss: 0.04, derivatives: [0.0, 0.0]});
+				equalFloat(loss.total(), 0.0, "total loss");*/
+			});
+		});
+	});
 });
 
 
-function equalFloat(actual, expected) {
+function equalFloat(actual, expected, message) {
 	if (Math.abs(actual - expected) > 0.00001) {
-		assert.equal(expected, actual);
+		assert.equal(expected, actual, message);
+	}
+}
+
+function equalLoss(actual, expected) {
+	equalFloat(actual.loss, expected.loss, "loss");
+	equalFloat(actual.derivatives.length, expected.derivatives.length, "count of loss derivatives");
+	for(var i = 0; i < actual.derivatives.length; i++) {
+		equalFloat(actual.derivatives[i], expected.derivatives[i], "derivative of [" + i + "]");
 	}
 }

61b6dafce249a16b90f13ed757bcb966053357c0
Denys Viliuzhanin <dvilyuzhanin@gmail,com>
2017-02-25 22:47:38 +0200

    Function

--- public/app/test/computation.test.js
+++ public/app/test/computation.test.js
@@ -70,78 +70,67 @@ describe('computation', () => {
 	});
 	describe('derivative', () => {
 		it('y = const', () => {
-			function func(x) {
+			var dFunc = computation.define(function() {
 				return 10;
-			}
-			assert.equal(computation.derivative(func, 1), 0);
-			assert.equal(computation.derivative(func, 10), 0);
-			assert.equal(computation.derivative(func, -1), 0);
+			}).derivativeBy('x');
+		    assert.equal(dFunc({x: 1}), 0);
+		    assert.equal(dFunc({x: 10}), 0);
+		    assert.equal(dFunc({x: -1}), 0);
 		});
 		
 		it('y = x', () => {
-			function func(x) {
-				return x;
-			}
-			equalFloat(computation.derivative(func, 1), 1);
-			equalFloat(computation.derivative(func, 10), 1);
-			equalFloat(computation.derivative(func, -1), 1);
+			var dFunc = computation.define(function() {
+				return this.x;
+			}).derivativeBy('x');
+			
+			equalFloat(dFunc({x:10}), 1);
+			equalFloat(dFunc({x:0}), 1);
+			equalFloat(dFunc({x:-1}), 1);
+			
 		});
 		
 		it('y = 3 * x', () => {
-			function func(x) {
-				return 3 * x;
-			}
-			equalFloat(computation.derivative(func, 1), 3);
-			equalFloat(computation.derivative(func, 10), 3);
-			equalFloat(computation.derivative(func, -1), 3);
+			var dFunc = computation.define(function() {
+				return 3 * this.x;
+			}).derivativeBy('x');
+			
+			equalFloat(dFunc({x: 10}), 3);
+			equalFloat(dFunc({x: -1}), 3);
+			equalFloat(dFunc({x: 0}), 3);
+			
 		});
 		
 		it('y = 3 * x * x', () => {
-			function func(x) {
-				return 3 * x * x;
-			}
-			equalFloat(computation.derivative(func, 1), 3 * 2 * 1);
-			equalFloat(computation.derivative(func, 10), 3 * 2  * 10);
-			equalFloat(computation.derivative(func, -5), 3 * 2 * -5);
+			var dFunc = computation.define(function() {
+				return 3 * this.x * this.x;
+			}).derivativeBy('x');
+			
+			equalFloat(dFunc({x: 10}), 3 * 2 * 10);
+			equalFloat(dFunc({x: -1}), 3 * 2 * -1);
+			equalFloat(dFunc({x: 0}), 3 * 2 * 0);
+			
 		});
 		
 		it('y = exp(x)', () => {
-			function func(x) {
-				return Math.exp(x);
-			}
-			equalFloat(computation.derivative(func, 1), Math.exp(1));
-			equalFloat(computation.derivative(func, 5), Math.exp(5));
-			equalFloat(computation.derivative(func, 3), Math.exp(3));
-			equalFloat(computation.derivative(func, -3), Math.exp(-3));
-		});
-		
-		it('c = a + b', () => {
-			function func(x) {
-				return x[0] + x[1];
-			}
-			equalFloat(computation.derivative(func, [1, 2], 0), 1);
-			equalFloat(computation.derivative(func, [10, 20], 1), 1);
-			equalFloat(computation.derivative(func, [-1, 3], 0), 1);
-		});
-		
-		it('c = x * x * a + b', () => {
-			function func(args) {
-				return Math.pow(args[0], 2) * args[1] + args[2];
-			}
-			equalFloat(computation.derivative(func, [3, 2, 5], 0), 2 * 3 * 2);
-			equalFloat(computation.derivative(func, [3, 2, 5], 1), 3 * 3);
-			equalFloat(computation.derivative(func, [3, 2, 5], 2), 1);
+			var dFunc = computation.define(function() {
+				return Math.exp(this.x);
+			}).derivativeBy('x');
+			
+			equalFloat(dFunc({x:1}), Math.exp(1));
+			equalFloat(dFunc({x:5}), Math.exp(5));
+			equalFloat(dFunc({x:3}), Math.exp(3));
+			equalFloat(dFunc({x:-3}), Math.exp(-3));
 		});
 		
 		it('sigmoid y = 1 / (1 + exp(-x))', () => {
-			function sigmoid(arg) {
-				return 1 / (1 + Math.exp(-arg));
-			}
+			var sigmoid = computation.define(function() {
+				return 1 / (1 + Math.exp(-this.x))
+			});
+			var sigmoidDx = sigmoid.derivativeBy('x'); 
 			
-			equalFloat(computation.derivative(sigmoid, -10), sigmoid(-10) * (1 - sigmoid(-10)));
-			equalFloat(computation.derivative(sigmoid, 0), sigmoid(0) * (1 - sigmoid(0)));
-			equalFloat(computation.derivative(sigmoid, 0), 0.25);
-			equalFloat(computation.derivative(sigmoid, 100), sigmoid(100) * (1 - sigmoid(100)));
+			equalFloat(sigmoid({x:-10}), sigmoid({x:-10}) * (1 - sigmoid({x:-10})));
+			equalFloat(sigmoid({x: 0}), sigmoid({x: 0}) * (1 - sigmoid({x: 0})));
+			equalFloat(sigmoid({x: 100}), sigmoid({x: 100}) * (1 - sigmoid({x: 100})));
 		});
 	});
 	
@@ -193,8 +182,8 @@ describe('computation', () => {
 
 
 function equalFloat(actual, expected, message) {
-	if (Math.abs(actual - expected) > 0.00001) {
-		assert.equal(expected, actual, message);
+	if (isNaN(actual) || isNaN(expected) || Math.abs(actual - expected) > 0.00001) {
+		assert.equal(actual, expected, message);
 	}
 }
 
